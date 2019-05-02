fd406957ca222e77afea3322f68d8acfc6abae94
James <jolaroux@gmail.com>
2019-05-02 14:25:41 -0500

    fixing whitespace

--- main.js
+++ main.js
@@ -13,55 +13,56 @@ let mainWindow // saves a global reference to mainWindow so it doesn't get garba
 //if (pro)
 app.on('ready', createWindow) // called when electron has initialized
 
-
-
 // This will create our app window, no surprise there
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1200, 
-    height: 900, 
-    'web-preferences': {'plugins': true}});
-    
+    width: 1200,
+    height: 900,
+    'web-preferences': { 'plugins': true }
+  });
+
   // display the index.html file
-  mainWindow.loadURL(`file://${ __dirname }/src/HTML and CSS/index.html`)
-  
+  mainWindow.loadURL(`file://${__dirname}/src/HTML and CSS/index.html`)
+
 
   // open dev tools by default so we can see any console errors
   mainWindow.webContents.openDevTools()
-  
 
-  
+
   //BrowserWindow.addDevToolsExtension('/Users/jameslaroux/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/2.1.9_0')
-  
+
   //app.commandLine.appendSwitch("--enable-experimental-webkit-features", true)
-  
+
   var template = [{
     label: "Application",
     submenu: [
-        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-        { type: "separator" },
-        { label: "Reload", accelerator: "Command+R", click: function() { app.relaunch(); app.quit(); }},
-        { type: "separator" },
-        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-    ]},{
+      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+      { type: "separator" },
+      { label: "Reload", accelerator: "Command+R", click: function () { app.relaunch(); app.quit(); } },
+      { type: "separator" },
+      { label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
+    ]
+  }, {
     label: "View",
     submenu: [
-        { label: "Show/Hide Console", accelerator:"Ctrl+C", click: function() { !mainWindow.webContents.isDevToolsOpened() ? mainWindow.webContents.openDevTools() : mainWindow.webContents.closeDevTools() }}
-    ]}, {
+      { label: "Show/Hide Console", accelerator: "Ctrl+C", click: function () { !mainWindow.webContents.isDevToolsOpened() ? mainWindow.webContents.openDevTools() : mainWindow.webContents.closeDevTools() } }
+    ]
+  }, {
     label: "Edit",
     submenu: [
-        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-        { type: "separator" },
-        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-        { label: "Copy", accelerator: "Cmd+C", selector: "copy:" },
-        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-        ]}
-    ];
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "Cmd+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]
+  }
+  ];
 
-    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
-      
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
 
   mainWindow.on('closed', function () {
     mainWindow = null
